525090a90ceb11e8e42b18efb8761bc706cbf8d3
moseph04 <moetuckerwhite@gmail.com>
2023-02-08 02:00:24 +0000

    getDistance

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 (function(window, _) {
-    window.myLibraryName = window.myLibraryName || {
+    window.claudius = window.claudius || {
       numz: {},
-      phyz: {},
+      phyz: {
+        /**
+         * 
+         * @param {object} pointA
+         * @param {object} pointB
+         * @param {number} distanceX
+         * @param {number} distanceY
+         * @return {number} distance
+         * @param {number} pointB.x 
+         * @param {number} pointB.y 
+         * @param {number} pointA.x 
+         * @param {number} pointA.y
+         */
+        getDistance(pointA, pointB) {
+          const
+          distanceX = pointB.x - pointA.x,
+          distanceY = pointB.y - pointA.y,
+          distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+          return distance;
+        },
+      },
     };
   }(window, window._));
+
+  claudius.phyz.calcDistance();
