36ca4032e0352226e262206ad31fde7369738bd6
moseph04 <moetuckerwhite@gmail.com>
2023-03-15 00:19:07 +0000

    added functions to nums for radians degrees and angles

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 (function(window, _) {
     window.claudius = window.claudius || {
-      numz: {},
+      numz: {
+        /**
+         * 
+         * @param {object} pointA 
+         * @param {object} pointB 
+         * @param {number} degrees
+         * @param {number} radians
+         * @param {number} distanceX
+         * @param {number} distanceY
+         * @param {number} pointB.x 
+         * @param {number} pointB.y 
+         * @param {number} pointA.x 
+         * @param {number} pointA.y
+         * @return {number} degrees
+         * @return {number} radians
+         */
+        getAngleDegrees(pointA, pointB) {
+          const
+            distanceX = pointB.x - pointA.x,
+            distanceY = pointB.y - pointA.y,
+            radians = Math.atan2(distanceY, distanceX),
+            degrees = radians * 180 / Math.PI;
+            return degrees;
+        }, 
+
+          degreesToRadians(degrees) {
+            return degrees * Math.PI / 180;
+          },
+
+          radiansToDegrees(radians) {
+            return radians * 180 / Math.PI;
+          },
+      },
       phyz: {
         /**
          * 
